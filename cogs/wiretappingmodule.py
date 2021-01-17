82368eac08f63bf5b2dc4837bb1f735838a42663
Brendon Barnes <brenbrenofspace@gmail.com>
2021-01-17 03:25:47 -0500

    actually for real this time

--- cogs/wiretappingmodule.py
+++ cogs/wiretappingmodule.py
@@ -15,31 +15,51 @@ wiretapList = []
 class wiretap(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
-    
+
     @commands.command(name="startwiretap", aliases=["wiretap", "swt"], hidden=True)
     @has_permissions(administrator=True)
-    async def startWiretap(self, ctx, user: discord.User):
+    async def startWiretap(self, ctx, pinguser: discord.User):
         global wiretapList
         guild = ctx.message.guild
-        category = get(guild.categories, name="Tickets")
+        category = get(guild.categories, name="FISA Court")
+        categoryid = get(guild.categories, name="FISA Court").id
         overwrites = {guild.default_role: discord.PermissionOverwrite(read_messages=False)}
+        userid = pinguser.id
 
         if category is None:
-            category = await guild.create_category_channel("Wiretaps")
+            category = await guild.create_category_channel("FISA Court")
+            categoryid = get(guild.categories, name="FISA Court").id
 
-        if user.id in wiretapList:
+
+        if userid in wiretapList:
             await ctx.send("This user is already wiretapped.")
         else:
-            wiretapList.append(user.id)
-            channel = await guild.create_text_channel(str(user.id), category="Wiretaps", overwrites=overwrites)
+            wiretapList.append(userid)
+            await ctx.send("FISA Warrant Approved, Wiretap Opened.")
+            if get(guild.channels, name=str(userid)) is None:
+                await guild.create_text_channel(name=userid, category=category, overwrites=overwrites)
+
+
+    @commands.command(name="closewiretap", aliases=["endwiretap","cwt","ewt"], hidden=True)
+    @has_permissions(administrator=True)
+    async def endWiretap(self, ctx, pinguser: discord.User):
+        global wiretapList
+        if pinguser.id in wiretapList:
+            wiretapList.remove(pinguser.id)
+        else:
+            await ctx.send("That user is not wiretapped.")
 
     @commands.Cog.listener()
-    async def on_message(anymessage):
-        if message.author in wiretapList:
-            if not isinstance(message.channel, discord.DMChannel):
-                if get(guild.channels, name=str(anymessage.author.id)) is not None:
-                    sendchannel = get(guild.channels, name=str(anymessage.author.id))
-                    await sendchannel.send(anymessage.author.name+"sent:"+'"'+anymessage+'"'+"in "+anymessage.channel)
+    async def on_message(self, given_message):
+        guild = given_message.guild
+        if given_message.author.id in wiretapList:
+            #print("Message from wiretap flagged user.")
+            if not isinstance(given_message.channel, discord.DMChannel):
+                #print("AND not a DM.")
+                if get(guild.channels, name=str(given_message.author.id)) is not None:
+                    #print("AND user has an existing wiretap channel.")
+                    sendchannel = get(guild.channels, name=str(given_message.author.id))
+                    await sendchannel.send(str(given_message.author.name)+" sent: "+'`'+str(given_message.content)+'`'+" in "+str(given_message.channel.name))
 
 def setup(bot):
     bot.add_cog(wiretap(bot))
--- wiretapper.py
+++ wiretapper.py
@@ -1,66 +1,96 @@
-
-# This is the wiretapping module, commands are built to track messages of one particular user.
-
-#Import Stack
-from discord.channel import CategoryChannel
-from discord.ext import commands
-from discord.ext.commands import has_permissions
+# Import Stack
 import discord
-from discord.utils import get
-import discord.abc
+from discord.ext import commands
+import jthon
+from pathlib import Path
+import os
+from cogs.util.errors import NotContributor
 
-#Variables
-wiretapList = []
+# Variable Defs
+config = jthon.load('config')
+TOKEN = str(config.get("token")) #pulls the bot token from the hidden config file
+bot = commands.Bot(command_prefix="^")
 
-#Module Code
-class wiretap(commands.Cog):
-    def __init__(self, bot):
-        self.bot = bot
+# Function Defs
+def get_prefix(bot, message): #determines the prefix from the config file, if there isn't one, it defaults to "$"
+    prefix = config.get('prefix')
+    if not prefix:
+        prefix = '^'
+    return commands.when_mentioned_or(*prefix)(bot, message)
 
-    @commands.command(name="startwiretap", aliases=["wiretap", "swt"], hidden=True)
-    @has_permissions(administrator=True)
-    async def startWiretap(self, ctx, pinguser: discord.User):
-        global wiretapList
-        guild = ctx.message.guild
-        category = get(guild.categories, name="FISA Court")
-        categoryid = get(guild.categories, name="FISA Court").id
-        overwrites = {guild.default_role: discord.PermissionOverwrite(read_messages=False)}
-        userid = pinguser.id
+def load_some_cogs(): #loads all available extensions, can also be done manually when the admin cog is loaded
+    bot.startup_extensions = []
+    path = Path('./cogs')
+    for dirpath, dirnames, filenames in os.walk(path):
+        if dirpath.strip('./') == str(path):
+            for cog in filenames:
+                if cog.endswith('.py') and not cog.startswith('_'):
+                    extension = 'cogs.'+cog[:-3]
+                    bot.startup_extensions.append(extension)
 
-        if category is None:
-            category = await guild.create_category_channel("FISA Court")
-            categoryid = get(guild.categories, name="FISA Court").id
+    if __name__ == "__main__":
+        for extension in bot.startup_extensions:
+            try:
+                bot.load_extension(extension)
+                print(f'Loaded {extension}')
+            except Exception as e:
+                exc = f'{type(e).__name__}: {e}'
+                print(f'Failed to load extension {extension}\n{exc}')
 
+# Bot Events
+@bot.event
+async def on_connect():
+    print("Connecting...")
 
-        if userid in wiretapList:
-            await ctx.send("This user is already wiretapped.")
-        else:
-            wiretapList.append(userid)
-            await ctx.send("FISA Warrant Approved, Wiretap Opened.")
-            if get(guild.channels, name=str(userid)) is None:
-                await guild.create_text_channel(name=userid, category=category, overwrites=overwrites)
+@bot.event
+async def on_ready():
+    print(f"We have logged in as {bot.user.name}")
 
+@bot.event #Handles the two most common errors and a catch-all
+async def on_command_error(ctx, error):
+    if isinstance(error, commands.CommandOnCooldown):
+        embed = discord.Embed(title=f'Command: {ctx.command.name}', colour=discord.Colour(0xFF0000), description=f"{ctx.author.name}, you are on cooldown for this command for {error.retry_after:.2f}s")
+        await ctx.send(embed=embed)
+        return
 
-    @commands.command(name="closewiretap", aliases=["endwiretap","cwt","ewt"], hidden=True)
-    @has_permissions(administrator=True)
-    async def endWiretap(self, ctx, pinguser: discord.User):
-        global wiretapList
-        if pinguser.id in wiretapList:
-            wiretapList.remove(pinguser.id)
-        else:
-            await ctx.send("That user is not wiretapped.")
+    if isinstance(error, NotContributor):
+        e = discord.Embed(colour=discord.Colour(0xFF0000), description=f"{ctx.author.name}, you aren't a contributor.")
+        await ctx.send(embed=e)
+        return
 
-    @commands.Cog.listener()
-    async def on_message(self, given_message):
-        guild = given_message.guild
-        if given_message.author.id in wiretapList:
-            #print("Message from wiretap flagged user.")
-            if not isinstance(given_message.channel, discord.DMChannel):
-                #print("AND not a DM.")
-                if get(guild.channels, name=str(given_message.author.id)) is not None:
-                    #print("AND user has an existing wiretap channel.")
-                    sendchannel = get(guild.channels, name=str(given_message.author.id))
-                    await sendchannel.send(str(given_message.author.name)+" sent: "+'`'+str(given_message.content)+'`'+" in "+str(given_message.channel.name))
+    else:
+        e = discord.Embed(colour=discord.Colour(0xFF0000), description=f"{error}")
+        await ctx.send(embed=e)
 
-def setup(bot):
-    bot.add_cog(wiretap(bot))
+@bot.event # Handles DMs while bot is online
+async def on_message(message):
+    if isinstance(message.channel, discord.DMChannel):
+        if str(message.author) != "MSCHF Man#1788":
+            print(f'Private message from {message.author}: "{message.content}"')
+            l = open("DMLog.txt", "a")
+            l.write(f'\nPrivate message from {message.author}: "{message.content}"')
+            l.close()
+    await bot.process_commands(message)
+
+# Bot Commands
+@commands.is_owner() #command to change the prefix, must be owner of the bot, writes to config file so to be persistent
+@bot.command(aliases=['sp'])
+async def setprefix(ctx, prefix: str=None):
+    if not prefix or len(prefix) >= 3:
+        await ctx.send("Please provide the prefix you would like to use between 1-2 chars.")
+    else:
+        config['prefix'] = prefix
+        config.save()
+        await ctx.send(f'Prefix updated to: {prefix}')
+
+@commands.is_owner()
+@commands.command()
+async def shutdown(self, ctx): #fairly self explanatory, allows the owner to shutdown the bot from discord
+    await ctx.send("Command Received; Bot Shutdown Imminent.")
+    print("Shutdown Command issued from within Discord...")
+    exit()
+
+# Run Code
+bot = commands.Bot(command_prefix=get_prefix)
+load_some_cogs()
+bot.run(TOKEN, bot=True, reconnect=True)
